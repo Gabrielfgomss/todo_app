2c051e140a000a4ca5a5ff9fa8c68f01cc809e35
Gabriel <gabriel.gomes_fernandes@hotmail.com>
2023-06-15 14:11:27 -0300

    adicionando config ao server

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,6 +3,9 @@ import user from './userRoute.js';
 import task from './taskRoute.js';
 
 const routes = (app) => {
+  app.route('/').get((req, res) => {
+    res.status(200).send({ nome: 'API do Portfolio' });
+  });
   app.use(express.json(), user, task);
 };
 
